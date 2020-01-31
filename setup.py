8d915c51a13202630eebfcaf6ec30da6499f5bcd
Billy Ashmall <billy.ashmall@nasa.gov>
2020-01-31 16:09:03 -0600

    Updated pip description to match github

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="climateserv", # Replace with your own username
-    version="0.0.6",
+    version="0.0.11",
     author="Billy Ashmall",
     author_email="billy.ashmall@nasa.gov",
-    description="This is a package to access the ClimateSERV API",
-    long_description="This is a package to access the ClimateSERV API](https://climateserv.servirglobal.net/) ",
+    description="This is a package to access the ClimateSERV API](https://climateserv.servirglobal.net/) ",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/servir/ClimateSERVpy",
     packages=setuptools.find_packages(),
